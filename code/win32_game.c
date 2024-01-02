4a5056690890d213c206faa4a2a5eb780c634c20
JaredIsaacs <jaredisaacs6@gmail.com>
2024-01-02 00:27:54 -0500

    initial commit

---
+++ code/win32_game.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+  MessageBox(NULL, "This is a test", "Test", MB_OK|MB_ICONINFORMATION);
+
+  return(0);
+}
